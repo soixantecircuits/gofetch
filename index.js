f904a58224a07d0b74afd4701b9788074e9eb573
Louis Fourquet <louis@soixantecircuits.fr>
2017-01-23 14:59:27 +0100

    Added minimum value to interval argument + formatting description

--- index.js
+++ index.js
@@ -11,24 +11,26 @@ var id
 
 vorpal
   .command('fetch <destination> <interval> [keyword] [width] [height]')
-  .description('Starts downloading images from Flickr to a given <destination> folder.\nOne image is downloaded every <interval> seconds.\nYou can provide a [keyword] to download images matching a specific subject. If not supplying any keyword, you will get images matching the keyword "kitten".\nYou can provide [width] and [height] to change the images size, default size is 320x240')
+  .description('Starts downloading images from Flickr to a given <destination> folder.\n' +
+    'One image is downloaded every <interval> seconds (minimum is 1).\n' +
+    'You can provide a [keyword] to download images matching a specific subject. If not supplying any keyword, you will get images matching the keyword "kitten".\n' +
+    'You can provide [width] and [height] to change the images size, default size is 320x240')
   .action(function (args, callback) {
     var width = args.width || 320
     var height = args.height || 240
     var keyword = args.keyword || 'kitten'
+    var interval = Number(args.interval)
+    if (interval < 1) { interval = 1 }
 
     id = setInterval(() => {
-      var ws = fs.createWriteStream(path.join(args.destination, keyword + '_' + Date.now() + '.jpg'))
-      ws.on('error', err => {
+      var stream = fs.createWriteStream(path.join(args.destination, keyword + '_' + Date.now() + '.jpg'))
+      stream.on('error', err => {
         this.log('Error:', err)
         return callback()
       })
-      ws.on('response', res => {
-        if (res === 200) { this.log('Downloaded', ws.path) }
-      })
-
-      request('http://loremflickr.com/' + width + '/' + height + '/' + keyword).pipe(ws)
-    }, args.interval * 1000)
+      request('http://loremflickr.com/' + width + '/' + height + '/' + keyword).pipe(stream)
+      this.log('Downloaded', stream.path)
+    }, interval * 1000)
   })
   .cancel(function () {
     clearInterval(id)
