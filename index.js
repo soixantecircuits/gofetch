7d0bd79f6d4f6ba6a51233c97c65a90c8e3dc0a9
yael <yael@soixantecircuits.fr>
2017-05-04 12:45:12 +0200

    :gem: add gif fetch from giphy

--- index.js
+++ index.js
@@ -22,34 +22,73 @@ vorpal
     var width = args.width || 320
     var height = args.height || 240
     var keyword = args.keyword || 'kitten'
-    var interval = Number(args.interval)
-    if (interval < 1) { interval = 1 }
+    var interval = Number(args.interval || 1)
+    if (typeof interval !== 'number' || interval < 1) { interval = 1 }
 
     id = setInterval(() => {
-      
-      let filename = keyword + '_' + Date.now() + '.jpg'
-      let finalDestinationPath = path.join(args.destination, filename)
-      let tempDestinationPath = path.join(tempDir, filename)
-      var stream = fs.createWriteStream(tempDestinationPath)
-      stream.on('error', err => {
-        this.log('Error:', err)
-        return callback()
-      })
-      .on('finish', () => {
-        fs.move(tempDestinationPath, finalDestinationPath, (err) => {
-          if (err) {
-            console.log(err)
-          }
-        })
-      })
-      request('http://loremflickr.com/' + width + '/' + height + '/' + keyword).pipe(stream)
-      this.log('Downloading ', stream.path)
+      try {
+        return downloadImageToFolder(`http://loremflickr.com/${width}/${height}/${keyword}`, args.destination, keyword, '.jpg')
+      } catch (err) {
+        return console.error(err)
+      }
     }, interval * 1000)
   })
   .cancel(function () {
     clearInterval(id)
   })
 
+vorpal.command('gif <destination> [interval] [keyword]')
+  .description('Download a gif with a tag <keyword> from giphy to a given <destination> folder every <interval>.\n' +
+    '<interval> must be at least 1.\n')
+  .action(function (args, callback) {
+    var keyword = args.keyword || 'kitten'
+    var interval = Number(args.interval || 1)
+    if (typeof interval !== 'number' || interval < 1) { interval = 1 }
+
+    id = setInterval(() => {
+      request(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${keyword}`, (err, res ,body) => {
+        if (err) {
+          return console.error(err)
+        }
+        try {
+          let dataObj = JSON.parse(body)
+          return downloadImageToFolder(dataObj.data.image_url, args.destination, keyword, '.gif')
+        } catch (error) {
+          return console.error(error)
+        }
+      })
+    }, interval * 1000)
+  })
+  .cancel(function () {
+    clearInterval(id)
+    vorpal.show()
+  })
+
+
 vorpal
   .delimiter('fetch-images$')
   .show()
+
+function downloadImageToFolder(url, destinationDir, keyword, extension) {
+      let filename = `${keyword}_${Date.now()}${extension}`
+      let finalDestinationPath = path.join(destinationDir, filename)
+      let tempDestinationPath = path.join(tempDir, filename)
+
+
+      var stream = fs.createWriteStream(tempDestinationPath)
+      stream.on('error', err => {
+        console.error('Error:', err)
+        return callback()
+      })
+      .on('finish', () => {
+        fs.move(tempDestinationPath, finalDestinationPath, (err) => {
+          if (err) {
+            console.error(err)
+          } else {
+            console.log('Downloaded ', stream.path)
+          }
+        })
+      })
+      request(url).pipe(stream)
+      console.log('Downloading ', stream.path)
+    }
