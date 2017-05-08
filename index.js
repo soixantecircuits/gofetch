f90442614ae4fd90dd880af68e70fcc4a4fc90d6
gabrielstuff <gabriel@soixantecircuits.fr>
2017-05-08 23:02:28 +0200

    💎📦 simplify fetch option and add standard-settings. Fixed #1

--- index.js
+++ index.js
@@ -7,28 +7,34 @@ const request = require('request')
 const fs = require('fs-extra')
 const path = require('path')
 const mkdirp = require('mkdirp')
+const standardSettings = require('standard-settings')
+const logUpdate = require('log-update')
 
-var tempDir = '/tmp'
+const settings = standardSettings.getSettings()
+
+var minInterval = 1
+var tempDir = settings.tmpDir
 mkdirp(tempDir)
 var id
+let intervals = []
 
 vorpal
-  .command('fetch <destination> <interval> [keyword] [width] [height]')
+  .command('fetch [keyword] [interval] [width] [height] [destination]')
   .description('Starts downloading images from Flickr to a given <destination> folder.\n' +
     'One image is downloaded every <interval> seconds (minimum is 1).\n' +
     'You can provide a [keyword] to download images matching a specific subject. If not supplying any keyword, you will get images matching the keyword "kitten".\n' +
     'You can provide [width] and [height] to change the images size, default size is 320x240')
-  .action(function (args, callback) {
-    var width = args.width || 320
-    var height = args.height || 240
-    var keyword = args.keyword || 'kitten'
-    var interval = Number(args.interval)
-    if (interval < 1) { interval = 1 }
-
-    id = setInterval(() => {
-      
+  .action(({ keyword, interval, width, height, destination}, callback) => {
+    width = width || settings.image.width
+    height = height || settings.image.height
+    keyword = keyword || settings.keyword
+    interval = Number(interval) || minInterval
+    var destinationPath = destination || settings.destinationPath
+    mkdirp(destinationPath)
+    if (interval < minInterval) { interval = minInterval }
+    intervals[`${keyword}`] = setInterval(() => {  
       let filename = keyword + '_' + Date.now() + '.jpg'
-      let finalDestinationPath = path.join(args.destination, filename)
+      let finalDestinationPath = path.join(destinationPath, filename)
       let tempDestinationPath = path.join(tempDir, filename)
       var stream = fs.createWriteStream(tempDestinationPath)
       stream.on('error', err => {
@@ -42,12 +48,31 @@ vorpal
           }
         })
       })
-      request('http://loremflickr.com/' + width + '/' + height + '/' + keyword).pipe(stream)
-      this.log('Downloading ', stream.path)
+      request(`http://loremflickr.com/${width}/${height}/${keyword}`).pipe(stream)
+      //this.log('Downloading ', )
+      vorpal.ui.redraw(`downloading ${finalDestinationPath}`)
     }, interval * 1000)
+    return callback()
   })
-  .cancel(function () {
-    clearInterval(id)
+
+vorpal
+  .command('stop <fetchName>', 'Stop fetching specific named fetcher.')
+  .action(({fetchName}, callback) => {
+    if(intervals[fetchName]){
+      clearInterval(intervals[fetchName])
+      vorpal.log(`Stop fetch: ${fetchName}`)
+    } else {
+      vorpal.log(`${fetchName} unknown.`)
+    }
+    return callback()
+  })
+
+vorpal
+  .command('list', 'List current fetcher.')
+  .action((args, callback) => {
+    vorpal.log('Here are your fetch:')
+    vorpal.log(Object.keys(intervals).join(' | '))
+    return callback()
   })
 
 vorpal
